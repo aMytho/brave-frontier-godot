2d2036165a77d966102c9bcfd460414bc97c74ac
MythoAway7 <58316242+MythoAway7@users.noreply.github.com>
2023-03-18 13:47:55 -0500

    Begin adding stages and dungeons

--- Area/Stages/stage_base.gd
+++ Area/Stages/stage_base.gd
@@ -1,7 +1,13 @@
 extends Resource
 class_name Stage
 
+@export_category("General")
 @export var id: int = 0
-
 @export var is_boss: bool = false
-@export var monsters = []
+@export var monsters: Array[Unit] = []
+
+
+@export_category("Mimics")
+@export var mimic_count: int = 0
+@export var mimic_chance: float = 0.0
+@export var minic_types: Array = []
