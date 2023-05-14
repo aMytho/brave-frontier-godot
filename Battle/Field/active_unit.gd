9b73159d3dcb82a768709c2debd9ca0e5b42beaf
MythoAway7 <58316242+MythoAway7@users.noreply.github.com>
2023-05-14 13:12:55 -0500

    Start turn logic, listen for actions, animations

--- Battle/Field/active_unit.gd
+++ Battle/Field/active_unit.gd
@@ -1,6 +1,10 @@
 extends AnimatedSprite2D
 
-
+signal AttackFinished(id: int)
+# Flag to let parent scenes check if a unit is in this slot
+@export var is_unit = false
+# Corresponds to the unit placement in unit_display
+@export var place_ID: int = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -11,7 +15,7 @@ func attack():
 	# to do - move torwards enemy
 	play("Attack")
 
-
 func _on_animation_finished():
 	#attack finished, switch to idle
 	play("Idle")
+	emit_signal("AttackFinished", place_ID)
--- Battle/battle.gd
+++ Battle/battle.gd
@@ -6,13 +6,24 @@ extends Node2D
 		$BG.texture = zone.BG
 
 @export var units: Array[Unit] = [null, null, null, null, null, null]
-@export var enemies: Array[Unit] = []
+
+@export_category("Turn Logic")
+@export var player_turn = true
+@export var enemy_turn = false
+@export var units_attacked: int = 0
+@export var enemies_attacked: int = 0
+@export var total_enemies: int = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	#Pass in the units
+	# Load the music
+	$BattleOST.stream = zone.music
+	$BattleOST.play()
+	
+	# Pass in the friendly units
 	$BattleUI.units = units
 	
+	# Connect attack buttons
 	$BattleUI/Unit1.Attack.connect(_unit_attack)
 	$BattleUI/Unit2.Attack.connect(_unit_attack)
 	$BattleUI/Unit3.Attack.connect(_unit_attack)
@@ -20,16 +31,80 @@ func _ready():
 	$BattleUI/Unit5.Attack.connect(_unit_attack)
 	$BattleUI/Unit6.Attack.connect(_unit_attack)
 	
-	# get the parent node
-	var battle_UI = $Friendlies
+	# Get the friendly node
+	var freindly_units = $Friendlies
 	# Display each unit on the field and start idle animations
-	for i in range(battle_UI.get_child_count()):
+	for i in range(freindly_units.get_child_count()):
+		# Ensure the unit exists
 		if i < units.size() and units[i] != null:
-			var child_node = battle_UI.get_child(i)
+			var child_node = freindly_units.get_child(i)
 			child_node.sprite_frames = units[i].sprite_sheet
 			child_node.play("Idle")
+	# Setup the environment
+	load_next_stage(0)
 
 func _unit_attack(unit_place: int):
-	var path = str("./Friendlies/Unit", unit_place)
-	print(path)
-	get_node(path).attack()
+	# Play attack animation, listen for anim end
+	get_node(str("./Friendlies/Unit", unit_place)).attack()
+	get_node(str("./Friendlies/Unit", unit_place)).connect("AttackFinished", _unit_attack_finished)
+
+func _unit_attack_finished(unit_place: int):
+	# Disconnect the signal
+	get_node(str("./Friendlies/Unit", unit_place)).disconnect("AttackFinished", _unit_attack_finished)
+	# Check if the turn is over
+	units_attacked = units_attacked + 1
+	check_if_player_turn_complete()
+
+func check_if_player_turn_complete():
+	# If the player has had all units attack, end turn
+	if units_attacked == get_unit_count():
+		print("Ready for enemy turn")
+		player_turn = false
+		enemy_turn = true
+		units_attacked = 0
+		run_enemy_turn()
+
+func run_enemy_turn():
+	print("Enemy turn!")
+	for enemy in $Enemies.get_children():
+		# Only play animation for units that exist
+		if enemy.is_unit:
+			enemy.attack()
+			enemy.connect("AttackFinished", _enemy_attack_finished)
+
+func _enemy_attack_finished(unit_place: int):
+	# Disconnect the signal
+	get_node(str("./Enemies/Unit", unit_place)).disconnect("AttackFinished", _enemy_attack_finished)
+	# Check if the turn is over
+	enemies_attacked = enemies_attacked + 1
+	if (enemies_attacked == total_enemies):
+		print("Enemy turn over")
+		enemy_turn = false
+		player_turn = true
+		enemies_attacked = 0
+		$BattleUI.release_attack_lockout()
+
+func load_next_stage(stage: int):
+	print("Next stage")
+	var counter = 0
+	var enemy_units = $Enemies
+	for unit in zone.Stage[stage].monsters:
+		print(unit)
+		total_enemies = total_enemies + 1
+		# Add enemy to field, start idle animation
+		var child_node = enemy_units.get_child(counter)
+		child_node.sprite_frames = unit.sprite_sheet
+		# Let this scene know that a unit is in this spot
+		child_node.is_unit = true
+		# Face the friendlies
+		child_node.flip_h = true
+		child_node.play("Idle")
+		counter = counter + 1
+
+func get_unit_count():
+	# returns how many non null units exist (down or active)
+	var counter = 0 
+	for unit in units:
+		if unit != null:
+			counter = counter + 1
+	return counter
