8f8e0e0abfd1f57bd016020848d1fd24c4141387
MythoAway7 <58316242+MythoAway7@users.noreply.github.com>
2023-05-16 14:40:11 -0500

    Add ability to pick random unit

--- Battle/Field/Enemies.gd
+++ Battle/Field/Enemies.gd
@@ -18,3 +18,24 @@ func _on_target_selected(id: int):
 	
 	# Set the current target
 	current_target = id
+
+func get_target_position():
+	# Returns the target unit's position or a random unit if none is selected
+	if current_target == 0:
+		return get_random_target()
+	return get_child(current_target - 1).position
+
+func get_random_target():
+	var children = get_children()
+	
+	# Remove any child that isn't a unit (empty slot)
+	var counter = 0
+	while counter < children.size():
+		if children[counter].is_unit == false:
+			children.remove_at(counter)
+		else:
+			# Only increment if a unit is found, otherwise it isn't accurate (children.size() updates when removed)
+			counter += 1
+
+	# Pick a random unit
+	return children[randi() % children.size()].position
