1d0425cb67d7e61f1c8a790fdc7f8e1ba0811479
Jonathan Shull <58316242+aMytho@users.noreply.github.com>
2023-05-17 17:25:51 -0500

    Update main_menu.gd

--- Menu/main_menu.gd
+++ Menu/main_menu.gd
@@ -4,6 +4,8 @@ extends Control
 func _ready():
 	print("Menu Ready")
 	get_tree().root.get_node("Game").get_child(0).emit_signal("startPlaying", "res://Music/1-02 The Summoner.mp3")
+	# Load the home screen
+	$content_switcher.load_scene_home("res://Menu/SubMenu/Home/home.tscn")
 
 func _on_footer_btn_clicked(id:int):
 	match id:
