0d37ddcb7b0dc01007bcf098ce5b80a22f7be410
MythoAway7 <58316242+MythoAway7@users.noreply.github.com>
2023-05-16 21:38:51 -0500

    Add transition scene

---
+++ Battle/UI/Transitions/transition_content.gd
@@ -0,0 +1,29 @@
+extends ColorRect
+
+@export var stage: int = 0
+@export var total_stages: int = 0
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+func set_properties(new_stage, max_stages, dungeon_name: String, zone_name: String):
+	# Sets the initial properties without showing the animations
+	stage = new_stage
+	total_stages = max_stages
+	
+	# Set the labels
+	$CurrentProgressLbl.text = str(new_stage)
+	$TotalProgressLbl.text = str(max_stages)
+	# Set the bar. Don't show animation yet]
+	$ProgressBar.max_value = max_stages
+	$ProgressBar.value = new_stage
+	# Set the stage names
+	$Name/Dungeon.text = dungeon_name
+	$Name/Stage.text = zone_name
+
+func update_progress(stage: int):
+	print("Setting transition value to ", stage)
+	$CurrentProgressLbl.text = str(stage)
+	# To-do: Animate the bar
+	$ProgressBar.value = stage
