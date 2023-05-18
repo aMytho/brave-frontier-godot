ba8e543340b43bcecccae5c12f841469aa7c84ab
MythoAway7 <58316242+MythoAway7@users.noreply.github.com>
2023-05-17 20:36:46 -0500

    Don't display a background if no unit is selected

--- Menu/SubMenu/Home/character_frame.gd
+++ Menu/SubMenu/Home/character_frame.gd
@@ -10,17 +10,25 @@ func _ready():
 	set_element(element)
 
 func set_element(elem: String):
-	#Holds the paths for the frames
+	if elem == "none":
+		# If no element is selected, load no background
+		$anim1.texture = null
+		$anim2.texture = null
+		$anim3.texture = null
+		$anim4.texture = null
+		return
+	
+	# Holds the paths for the frames
 	var paths = []
 	
-	#Sets the paths to the frames
+	# Sets the paths to the frames
 	var beginPath = "res://Menu/SubMenu/Home/Characters/Glows/"
 	paths.append(str(beginPath, elem, "_", 1, ".png"))
 	paths.append(str(beginPath, elem, "_", 2, ".png"))
 	paths.append(str(beginPath, elem, "_", 3, ".png"))
 	paths.append(str(beginPath, elem, "_", 4, ".png"))
 	
-	#Display them
+	# Display them
 	$anim1.texture = ResourceLoader.load(paths[0])
 	$anim2.texture = ResourceLoader.load(paths[1])
 	$anim3.texture = ResourceLoader.load(paths[2])
