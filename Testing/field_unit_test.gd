38f9a1d20b591074de54ed843b3ca684a61d5a40
MythoAway7 <58316242+MythoAway7@users.noreply.github.com>
2023-05-24 18:30:48 -0500

    Add new testing area for spriting

---
+++ Testing/field_unit_test.gd
@@ -0,0 +1,13 @@
+extends Node2D
+
+@onready var sprite = $"Field Unit"
+
+@export var unit: Unit = null
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	sprite.set_properties(unit.sprite_sheet, false, true, unit.char_equipment, unit.atk_equipment)
+
+
+func _on_button_pressed():
+	sprite.attack(Vector2(self.position.x - 200, self.position.y))
