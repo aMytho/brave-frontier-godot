a037ca1b05b49c1ea00a4565baeada2ba6a9f4ec
MythoAway7 <58316242+MythoAway7@users.noreply.github.com>
2023-06-05 21:52:06 -0500

    Add exp to player, show animations, use bar

--- Battle/Recap/QuestClear/quest_clear.gd
+++ Battle/Recap/QuestClear/quest_clear.gd
@@ -4,7 +4,10 @@ signal RewardsComplete
 
 @export var zel: int = 0
 @export var karma: int = 0
+# The total amount of xp
 @export var xp: int = 0
+# The amount of xp added so far. We keep track of this for the xpcurrentamount label
+@export var exp_added: int = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -15,11 +18,13 @@ func _ready():
 	move_zel()
 
 func move_zel():
+	# Show the zel
 	var zel_tween = create_tween()
 	zel_tween.tween_property($ZelContainer, "position", Vector2(0,0), 1.0)
 	zel_tween.tween_callback(animate_zel)
 
 func animate_zel():
+	# Wait, then animate the value
 	await get_tree().create_timer(1.0).timeout
 	$LabelAnimator.set_properties(0, zel, $ZelContainer/ZelAmount)
 	var zel_tween = create_tween()
@@ -27,11 +32,13 @@ func animate_zel():
 	zel_tween.tween_callback(move_karma)
 
 func move_karma():
+	# Show the karma
 	var karma_tween = create_tween()
 	karma_tween.tween_property($KarmaContainer, "position", Vector2(0,0), 1.0)
 	karma_tween.tween_callback(animate_karma)
 
 func animate_karma():
+	# Wait, then animate the value
 	await get_tree().create_timer(1.0).timeout
 	$LabelAnimator.set_properties(0, karma, $KarmaContainer/KarmaAmount)
 	var karma_twen = create_tween()
@@ -39,46 +46,81 @@ func animate_karma():
 	karma_twen.tween_callback(move_bar)
 
 func move_bar():
+	# Show the bar
 	var exp_tween = create_tween()
 	exp_tween.tween_property($XPContainer, "position", Vector2(0,0), 1.0)
-	exp_tween.tween_callback(animate_bar.bind(ActiveAccount.current_exp, xp, ActiveAccount.current_exp))
+	# Based on level overflow, animate the xp bar
+	if ActiveAccount.will_player_level_up(xp):
+		exp_tween.tween_callback(animate_bar.bind(
+			ActiveAccount.current_exp, ActiveAccount.max_exp - ActiveAccount.current_exp, true))
+	else:
+		exp_tween.tween_callback(animate_bar.bind(ActiveAccount.current_exp, xp, false))
 
-func animate_bar(initial_exp: int, exp_amount: int, bar_amount: int):
-	print(initial_exp, " ", exp_amount, " ", bar_amount)
+func animate_bar(initial_exp: int, bar_amount: int, is_overlfow: bool):
+	print(
+		"Animating XP bar with ", initial_exp, " as initial, ", bar_amount, " and is overflow:", is_overlfow
+		)
+	var max_exp = ActiveAccount.max_exp
 	# Wait a few seconds
 	await get_tree().create_timer(1.5).timeout
+	$content_switcher._set_blank_scene()
 	
 	# Set the initial values
-	$XPContainer/XPCurrentAmount.text = str(initial_exp)
-	$XPContainer/XPBar.value = bar_amount
+	$XPContainer/XPCurrentAmount.text = str(initial_exp + exp_added)
+	$XPContainer/XPNextAmount.text = str(max_exp - initial_exp)
+	$XPContainer/XPBar.max_value = max_exp
+	$XPContainer/XPBar.value = initial_exp
 	
 	# Animate the XP obtained label
-	$LabelAnimator.set_properties(0, exp_amount, $XPContainer/XPCurrentAmount)
+	$LabelAnimator.set_properties(initial_exp + exp_added, bar_amount, $XPContainer/XPCurrentAmount)
 	var tween_obtained = create_tween()
-	tween_obtained.tween_property($LabelAnimator, "current_val", exp_amount, 2.0)
+	tween_obtained.tween_property($LabelAnimator, "current_val", initial_exp + exp_added + bar_amount, 2.0)
 	
 	# Animate the xp to next lvl label
-	$LabelAnimator2.set_properties(exp_amount, 0, $XPContainer/XPNextAmount)
+	$LabelAnimator2.set_properties(max_exp - initial_exp, max_exp - initial_exp - bar_amount, $XPContainer/XPNextAmount)
 	var tween_next = create_tween()
-	tween_next.tween_property($LabelAnimator2, "current_val", 0, 2.0)
+	tween_next.tween_property($LabelAnimator2, "current_val", max_exp - initial_exp - bar_amount, 2.0)
 	
 	# Animate the exp bar
 	var tween = create_tween()
-	tween.tween_property($XPContainer/XPBar, "value", exp_amount, 2.0)
-	tween.tween_callback(check_for_level_up.bind(exp_amount))
-	# To-do: Check for level up, max level, etc
-	
-func check_for_level_up(exp_amount: int):
-	# Check for level up
-	if ActiveAccount.will_player_level_up(exp_amount):
-		print("Player leveled up")
+	tween.tween_property($XPContainer/XPBar, "value", bar_amount + initial_exp, 2.0)
+	tween.tween_callback(check_for_level_up.bind(bar_amount, is_overlfow))
+
+
+func check_for_level_up(exp_amount: int, is_overflow: bool):
+	print("EXP bar animation completed, adding: ", exp_amount, " to player.")
+	# Didn't level up...
+	if !is_overflow:
+		print("The player won't level up")
+		ActiveAccount.add_exp(exp_amount)
+		_on_XP_awarded()
+	else:
+		# Level up!
+		print("The player leveled up")
 		var arg_names = ["new_level"]
-		var arg_vals = [3]
-		# To-do: ANimate the level text
+		var arg_vals = [ActiveAccount.level + 1]
+		# Load the animation for the level up event
 		$content_switcher._set_blank_scene()
 		$content_switcher.load_scene_with_props("res://Battle/Recap/QuestClear/LeveledUp/leveled_up.tscn", 0, arg_names, arg_vals)
-	else:
-		_on_XP_awarded()
+		# Add the exp to the DB.
+		# We have to add an extra one since exp_amount and current xp == max == not level up
+		ActiveAccount.add_exp(exp_amount + 1)
+		
+		# Adjust the decrement/increment
+		xp = xp - exp_amount
+		exp_added = exp_added + exp_amount
+		if exp_amount == 0:
+			# We awared all the exp!
+			_on_XP_awarded()
+		else:
+			# There is still exp left to add.
+			if ActiveAccount.will_player_level_up(xp):
+				$content_switcher.get_scene().connect("LevelsComplete",
+					animate_bar.bind(0, ActiveAccount.max_exp, true))
+			else:
+				# Show the next level
+				$content_switcher.get_scene().connect("LevelsComplete",
+					animate_bar.bind(0, xp, false))
 
 func _on_XP_awarded():
 	await get_tree().create_timer(2.0).timeout
