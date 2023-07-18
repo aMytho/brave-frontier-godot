de0051537d92d4a49407a50a78da05f6253c1671
MythoAway7 <58316242+MythoAway7@users.noreply.github.com>
2023-07-17 19:16:00 -0500

    Editor cleanup

--- Battle/UI/battle_ui.gd
+++ Battle/UI/battle_ui.gd
@@ -26,10 +26,10 @@ func setCharacter():
 
 func set_selected_enemy_health(enemy_unit: Resource):
 	selected_enemy = enemy_unit
-	var enemy_health_bar = $EnemyHealth/HPBar
 	var prcent_hp = int(float(selected_enemy.HP) / selected_enemy.max_HP * 100)
 	$EnemyHealth/HP.set_text("HP : %s%s" % [prcent_hp if 0 <= prcent_hp else 0, "%"])
 	$EnemyHealth/Name.text = selected_enemy.name
+	var enemy_health_bar = $EnemyHealth/HPBar
 	enemy_health_bar.max_value = selected_enemy.max_HP
 	enemy_health_bar.value = selected_enemy.HP
 
