ee37dc7d7a5c823cb5f261c9cf706bc58694062c
MythoAway7 <58316242+MythoAway7@users.noreply.github.com>
2023-11-11 17:03:38 -0600

    Don't allow duplicate summoner names

--- Introduction/name_entry.gd
+++ Introduction/name_entry.gd
@@ -10,13 +10,27 @@ func _ready():
 
 func _on_button_pressed():
 	var input = $TextEdit.text
-	if len(input) > 0:
+	if len(input) > 0 and name_is_new(input):
 		var tween = create_tween()
 		tween.tween_property(self, "modulate", Color(0,0,0,0), 4.0)
 		tween.tween_callback(emit_signal.bind("NameChosen", input))
+	else:
+		print("Invalid Name. None provided or already exists...")
+		$Error.visible = true
 
 func _on_text_edit_focus_entered():
 	if first_time == true:
 		first_time = false
 		var tween = create_tween()
 		tween.tween_property($Button, "modulate", Color(1,1,1,1), 1.5)
+
+
+func name_is_new(name: String):
+	var names = Database.query(
+		'SELECT COUNT(*) as count FROM player_state WHERE player_name = "%s"' % name.to_lower()
+	)
+	
+	if names[0]["count"] == 0:
+		return true
+	else:
+		return false
