5cf94db7c3bbbfe8734e5ea46492392370273674
MythoAway7 <58316242+MythoAway7@users.noreply.github.com>
2023-11-13 20:50:40 -0600

    Add custom frame set in test unit scene

--- Testing/field_unit_test.gd
+++ Testing/field_unit_test.gd
@@ -11,3 +11,15 @@ func _ready():
 
 func _on_button_pressed():
 	sprite.attack(Vector2(self.position.x - 200, self.position.y - 200))
+
+
+
+func _on_button_2_button_up():
+	var sp = $"Field Unit".get_child(0)
+	sp.stop()
+	sp.frame = int($TextEdit.text)
+	var atk_kids = sp.get_child(2)
+	for kid in atk_kids.get_children():
+		kid.stop()
+		kid.frame = int($TextEdit.text)
+	
