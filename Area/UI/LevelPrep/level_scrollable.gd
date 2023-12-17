a06c44ef2810ab48bfcf1a4a916108cad0284ba7
MythoAway7 <58316242+MythoAway7@users.noreply.github.com>
2023-12-17 13:56:49 -0600

    Hide non-visible zones on selector, add levels

--- Area/UI/LevelPrep/level_scrollable.gd
+++ Area/UI/LevelPrep/level_scrollable.gd
@@ -6,9 +6,14 @@ signal ZoneSelected(level)
 func _ready():
 	pass # Replace with function body.
 
+
 func set_level(dun: Dungeon):
 	#For each zone, display it
 	for zone in dun.zones:
+		# Don't show hidden zones (likely in development)
+		if zone.is_visible == false:
+			continue
+		
 		var selectable_level = ResourceLoader.load("res://Area/UI/level.tscn").instantiate()
 		selectable_level.setProperties(zone)
 		# When clicked, emit up
@@ -16,6 +21,7 @@ func set_level(dun: Dungeon):
 		get_child(0).add_child(selectable_level)
 	# Add space after the last item so it isn't cut off (probably a bug)
 	get_child(0).add_spacer(false)
+
 
 func _on_zone_selected(zone):
 	# Someone clicked a zone
